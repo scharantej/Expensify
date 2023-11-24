ff0d5346346319347eeb45f6ecafdbe03d386d6b
scharantej <119601237+scharantej@users.noreply.github.com>
2023-11-24 20:53:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,23 @@
  
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///expenses.db'
-app.config['SECRET_KEY'] = 'secret-key'
-
 db = SQLAlchemy(app)
 
 class Expense(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), nullable=False)
-    category = db.Column(db.String(50), nullable=False)
     amount = db.Column(db.Float, nullable=False)
+    category = db.Column(db.String(80), nullable=False)
     date = db.Column(db.Date, nullable=False)
 
     def __repr__(self):
-        return '<Expense %r>' % self.name
+        return '<Expense %r>' % self.id
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
+    name = db.Column(db.String(80), unique=True, nullable=False)
 
     def __repr__(self):
         return '<Category %r>' % self.name
@@ -28,22 +25,19 @@ class Category(db.Model):
 @app.route('/')
 def index():
     expenses = Expense.query.all()
-    categories = Category.query.all()
-    return render_template('index.html', expenses=expenses, categories=categories)
+    return render_template('index.html', expenses=expenses)
 
 @app.route('/add_expense', methods=['GET', 'POST'])
 def add_expense():
     if request.method == 'POST':
-        name = request.form['name']
-        category = request.form['category']
         amount = request.form['amount']
+        category = request.form['category']
         date = request.form['date']
 
-        expense = Expense(name=name, category=category, amount=amount, date=date)
+        expense = Expense(amount=amount, category=category, date=date)
         db.session.add(expense)
         db.session.commit()
 
-        flash('Expense added successfully!')
         return redirect(url_for('index'))
 
     return render_template('add_expense.html')
@@ -53,14 +47,15 @@ def edit_expense(expense_id):
     expense = Expense.query.get_or_404(expense_id)
 
     if request.method == 'POST':
-        expense.name = request.form['name']
-        expense.category = request.form['category']
-        expense.amount = request.form['amount']
-        expense.date = request.form['date']
+        amount = request.form['amount']
+        category = request.form['category']
+        date = request.form['date']
 
+        expense.amount = amount
+        expense.category = category
+        expense.date = date
         db.session.commit()
 
-        flash('Expense updated successfully!')
         return redirect(url_for('index'))
 
     return render_template('edit_expense.html', expense=expense)
@@ -68,33 +63,19 @@ def edit_expense(expense_id):
 @app.route('/delete_expense/<int:expense_id>')
 def delete_expense(expense_id):
     expense = Expense.query.get_or_404(expense_id)
-
     db.session.delete(expense)
     db.session.commit()
 
-    flash('Expense deleted successfully!')
     return redirect(url_for('index'))
 
-@app.route('/get_expenses')
-def get_expenses():
-    expenses = Expense.query.all()
-    return jsonify(expenses=[expense.to_dict() for expense in expenses])
-
-@app.route('/get_categories')
-def get_categories():
+@app.route('/categories')
+def categories():
     categories = Category.query.all()
-    return jsonify(categories=[category.to_dict() for category in categories])
+    return render_template('categories.html', categories=categories)
 
-@app.route('/get_spending_over_time')
-def get_spending_over_time():
-    expenses = Expense.query.all()
-    spending_over_time = {}
-    for expense in expenses:
-        if expense.date.month not in spending_over_time:
-            spending_over_time[expense.date.month] = 0
-        spending_over_time[expense.date.month] += expense.amount
-    return jsonify(spending_over_time=spending_over_time)
+@app.route('/reports')
+def reports():
+    return render_template('reports.html')
 
 if __name__ == '__main__':
-    db.create_all()
-    app.run(debug=True)
+    app.run()
